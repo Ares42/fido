da5bda093413528c8cf513290a71e0c91c690428
Hunter Larco <hunter@larcolabs.com>
2020-06-12 22:56:29 +0000

    first example of successful youtube injection!

---
+++ src/fido/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div :class="$style.Host">
+    fido
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style module lang="sass">
+.Host {
+}
+</style>
--- src/injector.js
+++ src/injector.js
@@ -1,1 +1,14 @@
-console.log(100, 'injector');
+import Vue from 'vue';
+
+import App from '@/src/fido/App';
+
+window.addEventListener('load', () => {
+  const container = document.getElementById('secondary-inner');
+  if (!container) {
+    console.error('Unable to inject Fido');
+  }
+
+  const root = document.createElement('div');
+  container.insertBefore(root, container.childNodes[0]);
+  new Vue({ render: (h) => h(App) }).$mount(root);
+});
